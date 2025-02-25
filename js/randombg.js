cc21db89fe4512a9af5f9e1b0bdd593a30d2dac5
LoscX-X <loscx@qq.com>
2025-02-25 13:30:14 +0800

    Site updated: 2025-02-25 13:30:14

---
+++ js/randombg.js
@@ -0,0 +1,11 @@
+// 随机背景图片数组，图片可以换成图床链接，注意最后一条后面不要有逗号
+var backimg = [
+    "url(/img/op.jpg)",
+    "url(/img/op3.jpg)",
+    "url(/img/op4.jpg)",
+  ];
+  // 获取背景图片总数，生成随机数
+  var bgindex = Math.floor(Math.random() * backimg.length);
+  // 重设背景图片
+  document.getElementById("web_bg").style.backgroundImage = backimg[bgindex];
+  
